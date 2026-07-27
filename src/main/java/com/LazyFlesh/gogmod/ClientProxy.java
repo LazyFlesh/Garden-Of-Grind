b13f4b815c0c732381e6dfbd9925a80314d101b0
Irgendwer <67506320+Irgendwer01@users.noreply.github.com>
2026-07-27 05:39:17 +0200

    Merge pull request #6 from Irgendwer01/main
    Add instant respawn

--- src/main/java/com/LazyFlesh/gogmod/ClientProxy.java
+++ src/main/java/com/LazyFlesh/gogmod/ClientProxy.java
@@ -1,8 +1,56 @@
 package com.LazyFlesh.gogmod;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiGameOver;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.util.ChatComponentText;
+import net.minecraftforge.client.event.GuiOpenEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import org.lwjgl.input.Keyboard;
+
+import cpw.mods.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.common.gameevent.InputEvent;
+
 public class ClientProxy extends CommonProxy {
 
-    // Override CommonProxy methods here, if you want a different behaviour on the client (e.g. registering renders).
-    // Don't forget to call the super methods as well.
+    private boolean instantRespawn = false;
+    public KeyBinding toggleInstantRespawn;
 
+    @Override
+    public void preInit(FMLPreInitializationEvent event) {
+        super.preInit(event);
+        MinecraftForge.EVENT_BUS.register(this);
+        FMLCommonHandler.instance()
+            .bus()
+            .register(this);
+    }
+
+    @Override
+    public void init(FMLInitializationEvent event) {
+        super.init(event);
+        toggleInstantRespawn = new KeyBinding("gog.InstantRespawn.keydesc", Keyboard.KEY_END, "Garden of Grind");
+        ClientRegistry.registerKeyBinding(toggleInstantRespawn);
+    }
+
+    @SubscribeEvent
+    public void onKeyPressed(InputEvent.KeyInputEvent event) {
+        if (toggleInstantRespawn.isPressed()) {
+            instantRespawn = !instantRespawn;
+            Minecraft.getMinecraft().thePlayer
+                .addChatMessage(new ChatComponentText("Instant respawn: " + instantRespawn));
+        }
+    }
+
+    @SubscribeEvent
+    public void onOpenGui(GuiOpenEvent event) {
+        if (event.gui instanceof GuiGameOver && instantRespawn) {
+            event.setCanceled(true);
+            Minecraft.getMinecraft().thePlayer.respawnPlayer();
+        }
+    }
 }
